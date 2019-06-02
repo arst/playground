d2401e20d75514b4e008b871ed742b5c512cacd5
arst <artem.startsev@gmail.com>
2019-06-02 08:19:28 +0300

    Event source test

--- .netcore/.netcoreinaction/EventSourceConsumeProduce/EventSourceConsumeProduce/ConsoleEventListener.cs
+++ .netcore/.netcoreinaction/EventSourceConsumeProduce/EventSourceConsumeProduce/ConsoleEventListener.cs
@@ -5,6 +5,12 @@ namespace EventSourceConsumeProduce
 {
     public class ConsoleEventListener : EventListener
     {
+        protected override void OnEventSourceCreated(EventSource eventSource)
+        {
+            base.OnEventSourceCreated(eventSource);
+            Console.WriteLine("Event Source created");
+        }
+
         protected override void OnEventWritten(EventWrittenEventArgs eventData)
         {
             Console.WriteLine(eventData.Message, eventData.Payload[0]);
