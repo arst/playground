a4ccb2503e412c31e5ba4135b5647fca3c0ea095
arst <artem.startsev@gmail.com>
2019-06-02 08:18:18 +0300

    Event source test

---
+++ .netcore/.netcoreinaction/EventSourceConsumeProduce/EventSourceConsumeProduce/ConsoleEventListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace EventSourceConsumeProduce
+{
+    public class ConsoleEventListener : EventListener
+    {
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            Console.WriteLine(eventData.Message, eventData.Payload[0]);
+        }
+    }
+}
---
+++ .netcore/.netcoreinaction/EventSourceConsumeProduce/EventSourceConsumeProduce/SimpleEventSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace EventSourceConsumeProduce
+{
+    [EventSource(LocalizationResources = "EventSourceConsumeProduce.resources.EventSource")]
+    public class SimpleEventSource : EventSource
+    {
+        [Event(1)]
+        public void ReportOne(int report) => WriteEvent(1, report);
+
+        [Event(2)]
+        public void ReportTwo(string report) => WriteEvent(2, report);
+    }
+}
