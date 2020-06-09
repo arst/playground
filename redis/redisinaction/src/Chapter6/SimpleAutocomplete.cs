fda4a6b31f5b1136437add7a8ceb1519c99957ba
arst <artem.startsev@gmail.com>
2020-06-09 22:53:58 +0300

    Redis in Action: Chapter 6

---
+++ redis/redisinaction/src/Chapter6/SimpleAutocomplete.cs
@@ -0,0 +1,62 @@
+﻿using StackExchange.Redis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisInAction.Chapter6
+{
+    internal class SimpleAutocomplete
+    {
+        private readonly ConnectionMultiplexer connectionMultiplexer;
+
+        public SimpleAutocomplete(ConnectionMultiplexer connectionMultiplexer)
+        {
+            this.connectionMultiplexer = connectionMultiplexer;
+        }
+
+        private async Task<List<string>> GetContacts(string userName)
+        {
+            var database = connectionMultiplexer.GetDatabase();
+            var key = $"recent: {userName}";
+            var values = await database.ListRangeAsync(key);
+            var result = new List<string>();
+
+            foreach (var value in values)
+            {
+                result.Add(value.ToString());
+            }
+
+            return result;
+        }
+
+        private async Task AddOrUpdateContact(string userName, string contactName)
+        {
+            var database = connectionMultiplexer.GetDatabase();
+            var key = $"recent: {userName}";
+            await Task.WhenAll(
+                database.ListRemoveAsync(key, contactName),
+                database.ListLeftPushAsync(key, contactName),
+                database.ListTrimAsync(key, 0, 99));
+        }
+
+        private async Task RemoveContact(string userName, string contactName)
+        {
+            var database = connectionMultiplexer.GetDatabase();
+            var key = $"recent: {userName}";
+            await database.ListRemoveAsync(key, contactName, 0);
+        }
+
+        private async Task<List<string>> FetchAutoComplete(string userName, string prefix)
+        {
+            var database = connectionMultiplexer.GetDatabase();
+            var key = $"recent: {userName}";
+
+            var contacts = await database.ListRangeAsync(key);
+
+            return contacts
+                .Where(arg => arg.StartsWith(new RedisValue(prefix)))
+                .Select(arg => arg.ToString())
+                .ToList();
+        }
+    }
+}
---
+++ redis/redisinaction/src/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RedisInAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
